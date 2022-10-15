45d00abbcf8b3cdd1078b42beb9c862c1949d36a
shiva989-f <kumarshiva05722@gmail.com>
2022-10-15 17:41:36 +0530

    Commit

--- src/com/trishulMahadev/whileLoop.java
+++ src/com/trishulMahadev/whileLoop.java
@@ -1,7 +1,25 @@
 package com.trishulMahadev;
 
+import java.util.Scanner;
+
 public class whileLoop {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+
+        while (num >=0){
+            if (num % 2 == 0){
+                System.out.print("Even number");
+                break;
+            }
+            else {
+                System.out.print("Odd number");
+                break;
+            }
+        }
 
     }
 }
