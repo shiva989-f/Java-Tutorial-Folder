265b0569d3e15634ab15acf0beb343fc5fb953af
shiva989-f <kumarshiva05722@gmail.com>
2022-10-15 18:01:01 +0530

    Commit

---
+++ src/com/trishulMahadev/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.trishulMahadev;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int num = 0;
+
+        do {
+            ++num;
+            System.out.println(num);
+        }while (num < 10);
+
+    }
+}
---
+++ src/com/trishulMahadev/controlFLow.java
@@ -0,0 +1,18 @@
+package com.trishulMahadev;
+
+public class controlFLow {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                System.out.println("Skipping the value through continue statement");
+                continue;
+            } else if (i == 8) {
+                System.out.println("Breaking the loop using break statement");
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+
+    }
+}
